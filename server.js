0168dc1cbbb2f860e7d0634f15cbb1ec86890fa6
tiagomendonc <tiagomendoncahist@gmail.com>
2022-03-30 17:16:40 -0300

    Tentando resolver conexao banco de dados

--- server.js
+++ server.js
@@ -2,4 +2,5 @@ const http = require('http')
 const app = require('./app')
 const port = process.env.port || 3000
 const server = http.createServer(app)
+const mysql = require('mysql')
 server.listen(port)
